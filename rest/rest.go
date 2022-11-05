325ee68758b74f774ce814716a9eb56f179ab160
mulmuri <68294539+mulmuri@users.noreply.github.com>
2022-11-05 12:55:13 +0900

    add cors

--- rest/rest.go
+++ rest/rest.go
@@ -3,15 +3,26 @@ package rest
 import (
 	"net/http"
 	"github.com/gin-gonic/gin"
+	"github.com/gin-contrib/cors"
 )
 
 func RunAPI(address string) error {
 
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:3000", "https://mr-jackpot.run.goorm.io/"},
+		AllowMethods:     []string{"PUT", "PATCH", "POST", "OPTIONS"},
+		AllowCredentials: true,
+	}))
+
 	r.GET("/helloworld", func(c *gin.Context) {
-		c.String(http.StatusOK, "hello world")
+		c.String(http.StatusOK, "Hello World")
 	})
 
+	r.POST("/helloworld", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World")
+	})
 
 	return r.Run(address)
 }
